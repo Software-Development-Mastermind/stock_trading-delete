c54c25d3786f6c0b9b9c36e85e3105c822ed896f
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-09 15:00:12 -0700

    add path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar } from '@components/index'
 
 function App() {
   
   return (
     <>
+    <Navbar />
       <h1>Hello World</h1>
     </>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function NavbarComponent () {
+    return (
+        <Navbar className='shadow'>
+            <Nav.Item>
+                <Nav.Link href='/'>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href='/trade'>Trade</Nav.Link>
+            </Nav.Item>
+        </Navbar>
+    )
+}
+
+export default NavbarComponent
