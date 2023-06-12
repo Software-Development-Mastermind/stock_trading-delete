965c89079a1346a5752bc4b8fa0f7a87ffeae369
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-12 10:43:36 -0700

    add navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import '@styles/Navbar.css'
 
 function NavbarComponent () {
     return (
-        <Navbar className='shadow'>
-            <Nav.Item>
+        <Navbar className='navbar shadow'>
+            <Nav.Item className='nav-item'>
                 <Nav.Link href='/'>Home</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className='nav-item'>
                 <Nav.Link href='/holdings'>Holdings</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item className='nav-item'>
                 <Nav.Link href='/trade'>Trade</Nav.Link>
             </Nav.Item>
         </Navbar>
