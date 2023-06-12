5affd56bdf3183f8d9d276586812e8816a39c496
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-12 15:03:31 -0700

    fix API key formatting

--- server/src/lib/finnhub_api.py
+++ server/src/lib/finnhub_api.py
@@ -5,18 +5,21 @@ from dotenv import load_dotenv; load_dotenv()
 
 class FinnhubAPI():
 
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-Finnhub-Token': os.getenv('FINNHUB_API_KEY')
-            }
-        self.base_url = 'https://finnhub.io/api/v1/'
-        self.quote_url = self.base_url + 'quote?symbol='
-        self.company_url = self.base_url + 'stock/profile2?symbol='
+  def __init__(self) -> None:
+    self.session = requests.Session()
+    self.session.headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Finnhub-Token': os.getenv('FINNHUB_API_KEY')
+        }
+    self.base_url = 'https://finnhub.io/api/v1/'
+    self.quote_url = self.base_url + 'quote?symbol='
+    self.company_url = self.base_url + 'stock/profile2?symbol='
 
-    def get_quote(self, symbol):
-        url = self.quote_url + symbol
-        res = self.session.get(url)
-        return res.json()
+  def get_quote(self, symbol):
+    url = self.quote_url + symbol
+    res = self.session.get(url)
+    return res.json()
+    
+finnhub_api = FinnhubAPI()
+print(finnhub_api.get_quote('AAPL'))
