437e12cee646afbf8969db9e6d19b3a4432bf801
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-12 15:44:05 -0700

    add db connection parameters

--- server/src/lib/db_connection.py
+++ server/src/lib/db_connection.py
@@ -4,3 +4,8 @@ from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv; load_dotenv()
 
 class DBConnection:
+  
+  def __init__(self) -> None:
+    self.params = os.getenv('DB_CONNECTION_STRING')
+    self.conn = psycopg2.connect(self.params)
+    self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
