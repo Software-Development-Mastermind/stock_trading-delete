5fdb4a48e50e160a04b0077e3189dc1e8879434c
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-13 14:52:10 -0700

    install and implement SQLAlchemy

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_CONNECTION_STRING')
+db = SQLAlchemy()
+db.init_app(app)
 
 if __name__ == '__main__':
   app.run(debug=True)
