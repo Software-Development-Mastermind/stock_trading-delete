bf32dc8e851882a65f8faa6f193d3d88b225f633
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-13 15:54:31 -0700

    add file structure adjustments

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask
 from .extensions import db
+from src.routes import main
 
 
 def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_CONNECTION_STRING')
   db.init_app(app)
+
+  app.register_blueprint(main)
 
   return app
--- server/src/routes.py
+++ server/src/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 from server.src import db
-from server.src.models.users import User
+from server.src.models import User
+
+main = Blueprint('main', __name__)
 
 @main.route('/user', methods=['POST'])
 def create_user():
@@ -8,6 +10,6 @@ def create_user():
   new_user = User(email=data['email'], password=data['password'])
   db.session.add(new_user)
   db.session.commit()
-  
+
   return jsonify({'message': 'New user created!'}
 )
