df7296ce92f8b88b42b46d0f2b76a1dd188eb866
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-14 14:26:55 -0700

    add check for existing user to create_user method

--- server/src/routes.py
+++ server/src/routes.py
@@ -7,8 +7,16 @@ main = Blueprint('main', __name__)
 @main.route('/users', methods=['POST'])
 def create_user():
   data = request.get_json()
+  email = data['email']
+
+# Check if user already exists. If so, return 409 status code - conflict.
+
+  existing_user = Users.query.filter_by(email=email).first()
+  if existing_user:
+    return "A user with that email aleady exists!", 409
+
   new_user = Users(email=data['email'], password=data['password'])
   db.session.add(new_user)
   db.session.commit()
 
-  return "New user created!"
+  return "New user created!", 201
