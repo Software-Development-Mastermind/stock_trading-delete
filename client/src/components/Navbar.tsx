30059c615293de980e5f101479a6fb40199aa7f4
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-23 14:01:12 -0700

    add logout button and logic to Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+
+import AuthMethods from '@utils/index'
 import '@styles/Navbar.css'
 
 function NavbarComponent () {
+
+    const auth = new AuthMethods()
+    const navigate = useNavigate()
+
+    const handleLogout = async (e: any) => {
+        e.preventDefault();
+        auth.logout()
+        navigate('/login')
+    }
+    
     return (
         <Navbar className='navbar shadow'>
             <Nav.Item className='nav-item'>
@@ -14,6 +28,12 @@ function NavbarComponent () {
             <Nav.Item className='nav-item'>
                 <Nav.Link href='/trade'>Trade</Nav.Link>
             </Nav.Item>
+            <Nav.Item className='nav-item'>
+                <a onClick={handleLogout}> 
+                    Logout
+                </a>
+            </Nav.Item>
+            
         </Navbar>
     )
 }
