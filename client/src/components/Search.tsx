4fd7e8e52ca3c3e90e7ef76de7558610ec39e648
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-24 19:33:29 -0700

    add Search component styling

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -5,23 +5,19 @@ import '@styles/Search.css'
 function Search() {
   return(
     <div>
-      <Container className='search-container shadow-sm'>
-        <Row>
-            <Col sm={6}>
-              <Form className='d-flex'>
-                <Form.Control
-                  type='search'
-                  placeholder='Search'
-                  className='me-2 rounded-pill shadow'
-                  aria-label='Search'
-                >
-                </Form.Control>
-                <Button className='rounded-pill shadow' variant='primary'>
-                  Search
-                </Button>
-              </Form>
-            </Col>
-        </Row>
+      <Container className='search-container'>
+        <Form className='d-flex'>
+          <Form.Control
+            type='search'
+            placeholder='Search'
+            className='searchbar me-2 rounded-pill shadow-sm'
+            aria-label='Search'
+          >
+          </Form.Control>
+          <Button className='rounded-pill shadow-sm' variant='primary'>
+            Search
+          </Button>
+        </Form>
       </Container>
     </div>
   )
