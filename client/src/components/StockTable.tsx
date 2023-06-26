c601a5c2fd2c30b909e6a88de77eb6721b794048
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-25 20:06:44 -0700

    add type restrictions for table rows in StockTable component

--- client/src/components/StockTable.tsx
+++ client/src/components/StockTable.tsx
@@ -1,6 +1,33 @@
 
 
-function StockTable() {
+function StockTable({ stocks }) {
+
+  interface TableRowProps {
+    ticker: string;
+    name: string;
+  }
+
+  const TableRow = ({ ticker, name }: TableRowProps) => {
+    return (
+      <tr>
+        <td>{ticker}</td>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
+  const renderTableData = () => {
+    return stocks.map((stock, i) => {
+      const { ticker, name } = stock
+      return (
+        <tr key={i}>
+          <td>{ticker}</td>
+          <td>{name}</td>
+        </tr>
+      )
+    }))
+  }
+
   return(
       <>
       </>
