5b8f26750b93ce8cb3d919e9aa46d4e4b21a1e3d
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-07-08 12:25:32 -0700

    add Stock db model

--- server/src/models/portfolios.py
+++ server/src/models/portfolios.py
@@ -3,6 +3,11 @@ from src import db
 class Portfolio(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+  stocks = db.relationship('Stock', backref = 'portfolio', lazy = True)
+  cash = db.Column(db.Float, nullable = False)
+
+class Stock(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable = False)
   symbol = db.Column(db.String(10), nullable = False)
   shares = db.Column(db.Integer, nullable = False)
-  cash = db.Column(db.Float, nullable = False)
