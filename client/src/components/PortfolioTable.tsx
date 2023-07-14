9124308b1e7980243fbb3ea5ed1d01481b206e3e
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-07-13 17:24:37 -0700

    add PortfolioTable headers

--- client/src/components/PortfolioTable.tsx
+++ client/src/components/PortfolioTable.tsx
@@ -1,8 +1,55 @@
+import Axios from 'axios'
+import { useEffect, useState, useContext } from 'react'
+import { Table } from 'react-bootstrap'
+import { UserContext } from '@utils/index'
+
 
 function PortfolioTable() {
+
+  const user = useContext(UserContext)
+  const userId = user.id
+  const [holdings, setHoldings] = useState([])
+
+  useEffect(() => {
+    getUserPortfolio(userId)
+  })
+
+  const getUserPortfolio = async (userId: number) => {
+    const portfolioRes = await Axios.get(`/api/get_portfolio/${userId}`)
+    setHoldings([portfolioRes.data])
+  }
+
+  const renderPortfolioTable = () => {
+    return holdings.map((holding: any, i) => {
+      return (
+        <tr key={i}>
+          <td>{holding.symbol}</td>
+          <td>{holding.shares}</td>
+          <td>{holding.price}</td>
+          <td>{holding.total}</td>
+        </tr>
+      )
+    })
+  }
+
+
+
   return (
-    <>
-    </>
+    <Table>
+      <thead>
+        <tr>
+          <th>Ticker</th>
+          <th>Company</th>
+          <th>Shares</th>
+          <th>Cost</th>
+          <th>Current Value</th>
+          <th>Gain/Loss</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      {renderPortfolioTable()}
+    </Table>
   )
 }
 
--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -1,16 +1,16 @@
-import { useEffect, useContext } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { AuthMethods, UserContext } from '@utils/index'
 
-import Navbar from '@components/Navbar'
+import { AuthMethods } from '@utils/index'
+
 import Container from 'react-bootstrap/Container'
+import { Navbar, PortfolioTable } from '@/components/index'
 
 function Portfolio () {
 
   const navigate = useNavigate()
   const auth = new AuthMethods()
-  const user = useContext(UserContext)
-
+  
   useEffect(() => {
     if (!auth.loggedIn()) {navigate("/login")}
     }, []);
@@ -19,7 +19,8 @@ function Portfolio () {
       <div>
         <Navbar />
         <Container>
-          <h3>This is {user.email}'s portfolio page.</h3>
+          <h3>Holdings</h3>
+          <PortfolioTable />
         </Container>
       </div>
   )
