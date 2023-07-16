36e138364ccac0814d57e86ce96d5601d8967c64
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-07-16 12:32:45 -0700

    add TradeForm component styling and spacing

--- client/src/components/FinancialsTable.tsx
+++ client/src/components/FinancialsTable.tsx
@@ -11,11 +11,11 @@ function FinancialsTable({financials}: FinancialData) {
         <tbody>
           <tr>
             <td>52 Week High - {formatDate(financials["52WeekHighDate"])}:</td>
-            <td className='text-start'>{financials["52WeekHigh"] != null ? `$${financials["52WeekHigh"]}` : 'Temporarily unavailable.'}</td>
+            <td className='text-start'>{financials["52WeekHigh"] != null ? `$ ${financials["52WeekHigh"]}` : 'Temporarily unavailable.'}</td>
           </tr>
           <tr>
             <td>52 Week Low - {formatDate(financials["52WeekLowDate"])}:</td>
-            <td className='text-start'>{financials["52WeekLow"] != null ? `$${financials["52WeekLow"]}` : 'Temporarily unavailable.'}</td>
+            <td className='text-start'>{financials["52WeekLow"] != null ? `$ ${financials["52WeekLow"]}` : 'Temporarily unavailable.'}</td>
           </tr>
         </tbody>
       </Table>
--- client/src/components/TradeForm.tsx
+++ client/src/components/TradeForm.tsx
@@ -1,56 +1,81 @@
 import { useState } from 'react';
-import { Form, Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
+import { Row, Col, Form, Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
+
+import '@styles/TradeForm.css'
 
 
 function TradeForm({ selectedStock }: any) {
 
   const [checked, setChecked] = useState('buy')
+  const [shares, setShares] = useState(0)
 
   const handleToggleButtonChange = (value: string) => {
     setChecked(value);
   }
 
-  return(
-    <Form>
-      <ButtonGroup>
-        <ToggleButton
-          className='shadow-sm'
-          id='buy-button'
-          type='checkbox'
-          variant='outline-primary'
-          value='buy'
-          checked = {checked === "buy"}
-          onClick={() => {handleToggleButtonChange('buy')}}
-        >
-          Buy
-        </ToggleButton>
-        <ToggleButton
-          className='shadow-sm'
-          id='sell-button'
-          type='checkbox'
-          variant='outline-primary'
-          value='sell'
-          checked = {checked === "sell"}
-          onClick={() => {handleToggleButtonChange('sell')}}
-        >
-          Sell
-        </ToggleButton>
-      </ButtonGroup>
-      <Form.Group>
-        <Form.Label>{checked === 'sell' ? 'Shares to sell:' : 'Shares to buy:'}</Form.Label>
-        <Form.Control 
-          className='shadow-sm' 
-          type='number' 
-          placeholder='0' 
-          />
-        <p className ='mt-3'>{checked === 'sell' ? 'Total sale value:' : 'Total cost:'}</p>
-        <Button className='shadow-sm'>
-          {checked === 'sell' ? 'Complete sale' : 'Complete purchase'}
-        </Button>
-      </Form.Group>
-      
-    </Form>
-  )
+  const handleSharesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setShares(Number(e.target.value));
+  }
+
+  return (
+    <div className='trade-form-container shadow-sm'>
+      <Form>
+        <Row>
+          <Col xs={12} sm={2}>
+            <ButtonGroup className="mb-3">
+              <ToggleButton
+                className="shadow-sm"
+                id="buy-button"
+                type="checkbox"
+                variant="outline-primary"
+                value="buy"
+                checked={checked === 'buy'}
+                onClick={() => {
+                  handleToggleButtonChange('buy');
+                }}
+              >
+                Buy
+              </ToggleButton>
+              <ToggleButton
+                className="shadow-sm"
+                id="sell-button"
+                type="checkbox"
+                variant="outline-primary"
+                value="sell"
+                checked={checked === 'sell'}
+                onClick={() => {
+                  handleToggleButtonChange('sell');
+                }}
+              >
+                Sell
+              </ToggleButton>
+            </ButtonGroup>
+          </Col>
+          <Col xs={2} sm={2}>
+            <Form.Group className="mb-3">
+              <Form.Control 
+                className="shadow-sm" 
+                type="number" 
+                placeholder="0"
+                value={shares}
+                onChange = {handleSharesChange}
+                />
+            </Form.Group>
+          </Col>
+          <Col xs={8} sm={5} className='d-flex align-items-center'>
+            <p className='shares-identifier'>
+              shares of {selectedStock.name} ({selectedStock.symbol})
+            </p>
+          </Col>
+          </Row>
+            <p className='mt-1'>{checked === 'sell' ? 'Total sale value:' : 'Total cost:'}</p>
+            <p className='mt-1'>{checked === 'sell' ? 'Cash balance after sale:' : 'Cash balance after purchase:'}</p>
+          <Col xs={12}>
+            <Button className="shadow-sm">{checked === 'sell' ? 'Complete sale' : 'Complete purchase'}</Button>
+          </Col>
+      </Form>
+    </div>
+  );
 }
 
 export default TradeForm;
