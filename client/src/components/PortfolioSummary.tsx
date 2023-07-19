f43d733fecd5a2bd7c76296f674fdf688dfb9423
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-07-19 14:51:25 -0700

    refactor portfolio value calculation logic in PortfolioSummary component to use state

--- client/src/components/PortfolioSummary.tsx
+++ client/src/components/PortfolioSummary.tsx
@@ -1,23 +1,27 @@
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import { removeCommas, formatDollarAmount } from '@utils/index'
+import { get } from 'http';
 
 function PortfolioSummary({ holdings, userCash, isLoading }) {
 
-  const getTotalStockValue = () => {
+  const [totalPortfolioValue, setTotalPortfolioValue] = useState(0);
+
+  useEffect (() => {
+    getTotalPortfolioValue();
+  }, [holdings, userCash]);
+
+  const getTotalPortfolioValue = () => {
+    const cashToNumber = removeCommas(userCash);
     if (holdings && holdings.length > 0) {
       const totalValue = holdings.reduce((accumulator, holding) => {
         return accumulator + holding.currentValue;
       }, 0);
-      return totalValue;
+      const portfolio = formatDollarAmount(totalValue + cashToNumber);
+      setTotalPortfolioValue(portfolio);
     }
-    return 0;
   };
-
-  const totalStockValue = getTotalStockValue();
-  const cashToNumber = removeCommas(userCash);
-  const totalPortfolioValue = formatDollarAmount(totalStockValue + cashToNumber);
-
 
   return(
     <Container className='shadow-sm'>
@@ -26,7 +30,7 @@ function PortfolioSummary({ holdings, userCash, isLoading }) {
           <h5>Cash: $ {userCash} </h5>
         </Col>
         <Col>
-          <h5>Total Value: {totalPortfolioValue}</h5>
+          <h5>Total Value: $ {totalPortfolioValue}</h5>
         </Col>
         <Col>
           <h5>Total Gain/Loss:</h5>
