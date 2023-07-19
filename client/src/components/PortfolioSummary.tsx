db279a209b5f92331561f6c763a6f66612afab67
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-07-19 13:27:52 -0700

    add logic to calculate total portfolio value in PortfolioSummary component

--- client/src/components/PortfolioSummary.tsx
+++ client/src/components/PortfolioSummary.tsx
@@ -1,6 +1,24 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
+import { removeCommas, formatDollarAmount } from '@utils/index'
+
 function PortfolioSummary({ holdings, userCash, isLoading }) {
+
+  const getTotalStockValue = () => {
+    if (holdings && holdings.length > 0) {
+      const totalValue = holdings.reduce((accumulator, holding) => {
+        return accumulator + holding.currentValue;
+      }, 0);
+      return totalValue;
+    }
+    return 0;
+  };
+
+  const totalStockValue = getTotalStockValue();
+  const cashToNumber = removeCommas(userCash);
+  const totalPortfolioValue = formatDollarAmount(totalStockValue + cashToNumber);
+
+
   return(
     <Container className='shadow-sm'>
       <Row>
@@ -8,7 +26,7 @@ function PortfolioSummary({ holdings, userCash, isLoading }) {
           <h5>Cash: $ {userCash} </h5>
         </Col>
         <Col>
-          <h5>Total Value:</h5>
+          <h5>Total Value: {totalPortfolioValue}</h5>
         </Col>
         <Col>
           <h5>Total Gain/Loss:</h5>
